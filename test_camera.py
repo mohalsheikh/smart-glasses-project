1ed2955aa2b1875d11a289fe8088bd95e0e4ff23
mohalsheikh <143254315+mohalsheikh@users.noreply.github.com>
2025-09-25 20:30:32 -0700

    added the test camera

--- test_camera.py
+++ test_camera.py
@@ -1,6 +1,6 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)  # Try to open default webcam
 if not cap.isOpened():
     print("❌ Cannot access camera")
     exit()
@@ -12,10 +12,14 @@ while True:
     if not ret:
         print("⚠️ Failed to grab frame")
         break
+
     cv2.imshow("Test Camera", frame)
 
+    # Press Q to quit window
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 cap.release()
 cv2.destroyAllWindows()
+
+
