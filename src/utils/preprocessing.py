62c9253983f814ee3f5761a6c622b8505f383fdc
EthanScottWalden <EthanScott.Walden@calbaptist.edu>
2025-10-21 17:03:39 -0700

    added color channel swap functions to preprocessing.py

--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -1,10 +1,38 @@
 """
 Image preprocessing functions
-Created by Mohammed
-
-
-import cv2
-
-def to_gray(frame):
-    return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+Created by Ethan
 """
+
+import cv2 as cv
+
+# Convert a BGR image to grayscale.
+def bgr_to_gray(image):
+    return cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+# Convert a BGR image to HSV.
+def bgr_to_hsv(image):
+    return cv.cvtColor(image, cv.COLOR_BGR2HSV)
+
+# Convert a grayscale image to BGR.
+def gray_to_bgr(image):
+    return cv.cvtColor(image, cv.COLOR_GRAY2BGR)
+
+# Convert a HSV image to BGR.
+def hsv_to_bgr(image):
+    return cv.cvtColor(image, cv.COLOR_HSV2BGR)
+
+# Convert a RGB image to grayscale.
+def rgb_to_gray(image):
+    return cv.cvtColor(image, cv.COLOR_RGB2GRAY)
+
+# Convert a RGB image to HSV.
+def rgb_to_hsv(image):
+    return cv.cvtColor(image, cv.COLOR_RGB2HSV)
+
+# Convert a grayscale image to RGB.
+def gray_to_rgb(image):
+    return cv.cvtColor(image, cv.COLOR_GRAY2RGB)
+
+# Convert a HSV image to RGB.
+def hsv_to_rgb(image):
+    return cv.cvtColor(image, cv.COLOR_HSV2RGB)
